d8ac6ca943ece46a3e5c1fd52f35a9c0a442ada9
Seunghoon Lee <lsh2002@hotmail.com>
2021-07-26 13:39:27 -0400

    Some clean up

--- Seunghoon-Lee-P1/Controllers/HomeController.cs
+++ Seunghoon-Lee-P1/Controllers/HomeController.cs
@@ -23,11 +23,6 @@ namespace Seunghoon_Lee_P1.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
