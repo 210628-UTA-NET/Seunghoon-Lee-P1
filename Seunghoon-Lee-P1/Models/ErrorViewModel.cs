ba8cca12eb7b163442b415944ff7aa8461265a72
Seunghoon Lee <lsh2002@hotmail.com>
2021-07-26 13:33:23 -0400

    Created the project

---
+++ Seunghoon-Lee-P1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Seunghoon_Lee_P1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
