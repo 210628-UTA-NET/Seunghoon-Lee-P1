822852e4a563d114d0d3ae208d7bbd87e73309ae
Seunghoon Lee <lsh2002@hotmail.com>
2021-07-26 16:22:32 -0400

    Created db context

---
+++ Seunghoon-Lee-P1/Models/DataLayer/P1Context.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Seunghoon_Lee_P1.Models.DomainModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Seunghoon_Lee_P1.Models.DataLayer
+{
+    public class P1Context : DbContext
+    {
+        public P1Context(DbContextOptions<P1Context> p_options)
+            : base(p_options)
+        {
+        }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Inventory> Inventories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<State> States { get; set; }
+        public DbSet<Store> Stores { get; set; }
+    }
+}
