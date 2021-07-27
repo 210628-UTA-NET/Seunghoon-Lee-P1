d17b0fdff7b0b6408991af5584da257aa66e1bac
Seunghoon Lee <lsh2002@hotmail.com>
2021-07-27 07:08:44 -0400

    Added ProductDTO

---
+++ Seunghoon-Lee-P1/Models/DTOs/ProductGridDTO.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Seunghoon_Lee_P1.Models.DTOs
+{
+    public class ProductGridDTO : GridDTO
+    {
+        [JsonIgnore]
+        public const string DefaultFilter = "all";
+
+        public string Brand { get; set; } = DefaultFilter;
+        public string Category { get; set; } = DefaultFilter;
+        public string Price { get; set; } = DefaultFilter;
+
+
+    }
+}
