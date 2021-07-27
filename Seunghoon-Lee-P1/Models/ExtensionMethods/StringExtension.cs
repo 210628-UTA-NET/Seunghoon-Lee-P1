8b9b43ca9bd2f17e6393b83dc78ea86e2a1093ff
Seunghoon Lee <lsh2002@hotmail.com>
2021-07-27 05:34:58 -0400

    Added extension methods for string

---
+++ Seunghoon-Lee-P1/Models/ExtensionMethods/StringExtension.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seunghoon_Lee_P1.Models.ExtensionMethods
+{
+    public static class StringExtension
+    {
+        public static string Slug(this string s)
+        {
+            var sb = new StringBuilder();
+            foreach(char c in s)
+            {
+                if (c == '-' || !char.IsPunctuation(c))
+                {
+                    sb.Append(c);
+                }
+            }
+            return sb.ToString().Replace(' ', '-').ToLower();
+        }
+
+        public static bool EqualsNoCase(this string s, string sToCompare) =>
+            s?.ToLower() == sToCompare?.ToLower();
+
+        public static int ToInt(this string s)
+        {
+            int.TryParse(s, out int id);
+            return id;
+        }
+
+        public static string Capitalize(this string s) =>
+            s?.Substring(0, 1)?.ToUpper() + s?.Substring(1);
+    }
+}
