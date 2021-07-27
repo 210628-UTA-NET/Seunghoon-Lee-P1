35cd3b3bc0dc0e1743671c292bd64630a003f337
Seunghoon Lee <lsh2002@hotmail.com>
2021-07-27 14:58:59 -0400

    Added LineItemListExtension method

---
+++ Seunghoon-Lee-P1/Models/ExtensionMethods/LineItemListExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Seunghoon_Lee_P1.Models.DomainModels;
+using Seunghoon_Lee_P1.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Seunghoon_Lee_P1.Models.ExtensionMethods
+{
+    public static class LineItemListExtensions
+    {
+        public static List<LIneItemDTO> ToDTO(this List<LineItem> list) =>
+            list.Select(li => new LIneItemDTO
+            {
+                ProductId = li.Product.ProductId,
+                Quantity = li.Quantity
+            }).ToList();
+    }
+}
