0c74c89d07edc83e698851571cfb5857e94ec51a
Seunghoon Lee <lsh2002@hotmail.com>
2021-07-27 16:56:55 -0400

    Updated home controller

--- Seunghoon-Lee-P1/Controllers/HomeController.cs
+++ Seunghoon-Lee-P1/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Seunghoon_Lee_P1.Models;
+using Seunghoon_Lee_P1.Models.DataLayer;
+using Seunghoon_Lee_P1.Models.DataLayer.Repositories;
+using Seunghoon_Lee_P1.Models.DomainModels;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -13,6 +16,10 @@ namespace Seunghoon_Lee_P1.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
+        private Repository<Product> data { get; set; }
+        public HomeController(P1Context context) => data = new Repository<Product>(context);
+ 
+
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
@@ -20,7 +27,16 @@ namespace Seunghoon_Lee_P1.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var random = data.Get(new QueryOptions<Product>
+            {
+                OrderBy = p => Guid.NewGuid()
+            });
+            return View(random);
+        }
+
+        public ContentResult Register()
+        {
+            return Content("To do");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
