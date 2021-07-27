842130cc213d8c2fabef29af927040e67608cb81
Seunghoon Lee <lsh2002@hotmail.com>
2021-07-27 14:46:47 -0400

    Created LineItem

---
+++ Seunghoon-Lee-P1/Models/DTOs/LineItemDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Seunghoon_Lee_P1.Models.DTOs
+{
+    public class LIneItemDTO
+    {
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ Seunghoon-Lee-P1/Models/DomainModels/LineItem.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using Seunghoon_Lee_P1.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Seunghoon_Lee_P1.Models.DomainModels
+{
+    public class LineItem
+    {
+        public ProductDTO Product { get; set; }
+        public int Quantity { get; set; }
+
+        [JsonIgnore]
+        public double SubTotal => Product.Price * Quantity;
+
+    }
+}
