2b2c295f086e0508c5d5fe44314e0ed53bd0b63d
Seunghoon Lee <lsh2002@hotmail.com>
2021-07-27 06:34:52 -0400

    Added helper class - FilterPrefix

---
+++ Seunghoon-Lee-P1/Models/Grid/FilterPrefix.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Seunghoon_Lee_P1.Models.Grid
+{
+    public static class FilterPrefix
+    {
+        public const string Brand = "brand-";
+        public const string Category = "category-";
+        public const string Store = "store-";
+    }
+}
