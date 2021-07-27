8845adf8ee18458402e9e8e7fb7f47cdb8c80985
Seunghoon Lee <lsh2002@hotmail.com>
2021-07-27 03:35:32 -0400

    Created IRepository interface

---
+++ Seunghoon-Lee-P1/Models/DataLayer/Repositories/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Seunghoon_Lee_P1.Models.DataLayer.Repositories
+{
+    interface IRepository<T> where T : class
+    {
+        IEnumerable<T> List(QueryOptions<T> options);
+        int Count { get; }
+
+        T Get(QueryOptions<T> options);
+        T Get(int id);
+        T Get(string id);
+
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+        void Save();
+    }
+}
