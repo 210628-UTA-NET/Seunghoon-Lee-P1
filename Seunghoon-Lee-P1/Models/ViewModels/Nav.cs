82096ff278583c253473b7ba5eb138674b5a2d8d
Seunghoon Lee <lsh2002@hotmail.com>
2021-07-27 16:47:55 -0400

    Created Nav Class

---
+++ Seunghoon-Lee-P1/Models/ViewModels/Nav.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Seunghoon_Lee_P1.Models.ViewModels
+{
+    public static class Nav
+    {
+        public static string Active(string value, string current) =>
+            (value == current) ? "active" : "";
+        public static string Active(int value, int current) =>
+            (value == current) ? "active" : "";
+    }
+}
