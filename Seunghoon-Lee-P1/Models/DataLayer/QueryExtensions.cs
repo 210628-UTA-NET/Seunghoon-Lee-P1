c892e27a339dfe42043ffdd65bb36b5abf64ce91
Seunghoon Lee <lsh2002@hotmail.com>
2021-07-27 03:26:11 -0400

    Query options

--- Seunghoon-Lee-P1/Models/DataLayer/QueryExtensions.cs
+++ Seunghoon-Lee-P1/Models/DataLayer/QueryExtensions.cs
@@ -5,7 +5,13 @@ using System.Threading.Tasks;
 
 namespace Seunghoon_Lee_P1.Models.DataLayer
 {
-    public class QueryExtensions
+    public static class QueryExtensions
     {
+        public static IQueryable<T> PageBy<T>(this IQueryable<T> items, int pageNumber, int pageSize)
+        {
+            return items
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize);
+        }
     }
 }
